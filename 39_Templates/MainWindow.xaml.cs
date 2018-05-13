7fa76d0bacf1102f0c47806177b929ac23df60c1
Thuan <thuandev2706@gmail.com>
2018-05-13 12:18:15 +0700

    complete 37+38+39

---
+++ 39_Templates/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _39_Templates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btncontrolTempl_Click(object sender, RoutedEventArgs e)
+        {
+            W_ControlTemplate w = new W_ControlTemplate();
+            this.Hide();
+            w.ShowDialog();
+            this.Show();
+        }
+
+        private void btndataTempl_Click(object sender, RoutedEventArgs e)
+        {
+            W_DataTemplate w = new W_DataTemplate();
+            this.Hide();
+            w.ShowDialog();
+            this.Show();
+        }
+        
+    }
+}
---
+++ 39_Templates/W_DataTemplate.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _39_Templates
+{
+    /// <summary>
+    /// Interaction logic for W_DataTemplate.xaml
+    /// </summary>
+    public partial class W_DataTemplate : Window
+    {
+        Person src = new Person { Name = "Ali", Age = 27 };
+        List<Person> people = new List<Person>();
+        public W_DataTemplate()
+        {
+            InitializeComponent();
+            people.Add(src);
+            people.Add(new Person { Name = "Mike", Age = 62 });
+            people.Add(new Person { Name = "Brian", Age = 12 });
+            this.DataContext = people;
+        }
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            string message = src.Name + " is " + src.Age;
+            MessageBox.Show(message);
+        }
+        public class Person
+        {
+            private string nameValue;
+            public string Name
+            {
+                get { return nameValue; }
+                set { nameValue = value; }
+            }
+            private double ageValue;
+            public double Age
+            {
+                get { return ageValue; }
+                set
+                {
+                    if (value != ageValue)
+                    {
+                        ageValue = value;
+                    }
+                }
+            }
+        }
+    }
+}
